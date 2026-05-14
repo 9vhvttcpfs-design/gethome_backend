de64febec1b697ecc3924d5b868949abffa3a5a3
9vhvttcpfs-design <9vhvttcpfs@privaterelay.appleid.com>
2026-05-14 22:27:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,5 +17,4 @@ pool.connect((err) => {
 
 app.listen(10000, () => console.log('🚀 Server running on port 10000'));
 
-
 console.log("Checking connection to:", process.env.DATABASE_URL ? "URL Found" : "URL NOT FOUND");
