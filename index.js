8cf1f44846ca1f419ad54e2e41b18c52a6c560ec
medinat <medibrhm07@gmail.com>
2026-05-15 08:25:44 +0300

    fix: added cors

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const express = require('express');
+const cors = require('cors'); // 
+const app = express();
+
+app.use(cors()); //
 const cors = require('cors');
 app.use(cors());
 require('dotenv').config(); // This line reads your .env file
